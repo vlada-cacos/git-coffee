a5420ca0543a3ac9f826f0debfd22e4a5a34a93d
Vlada <v.murashkina10@mail.ru>
2026-08-30 17:20:47 +0300

    Change coffee aplication

--- main.py
+++ main.py
@@ -1,21 +1,84 @@
+import os
 import sqlite3
 import sys
 
-from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
-from PyQt5.QtWidgets import QApplication, QMainWindow
+
+from main_ui import Ui_MainWindow
+from addEditCoffeeForm import Ui_AddEditCoffeeForm
+
+
+if getattr(sys, "frozen", False):
+    BASE_DIR = os.path.dirname(sys.executable)
+else:
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DB_FILE = os.path.join(BASE_DIR, "data", "coffee.sqlite")
+
+
+class AddEditCoffeeDialog(QDialog):
+    def __init__(self, parent=None, coffee=None):
+        super().__init__(parent)
+
+        self.ui = Ui_AddEditCoffeeForm()
+        self.ui.setupUi(self)
+
+        self.coffee = coffee
+
+        self.ui.roastingComboBox.addItems([
+            "Светлая",
+            "Средняя",
+            "Темная"
+        ])
+
+        self.ui.typeComboBox.addItems([
+            "В зернах",
+            "Молотый"
+        ])
+
+        self.ui.saveButton.clicked.connect(self.accept)
+        self.ui.cancelButton.clicked.connect(self.reject)
+
+        if coffee is not None:
+            self.ui.nameEdit.setText(str(coffee[1]))
+            self.ui.roastingComboBox.setCurrentText(str(coffee[2]))
+            self.ui.typeComboBox.setCurrentText(str(coffee[3]))
+            self.ui.tasteEdit.setText(str(coffee[4]))
+            self.ui.priceSpinBox.setValue(float(coffee[5]))
+            self.ui.volumeSpinBox.setValue(int(coffee[6]))
+
+    def get_data(self):
+        return (
+            self.ui.nameEdit.text(),
+            self.ui.roastingComboBox.currentText(),
+            self.ui.typeComboBox.currentText(),
+            self.ui.tasteEdit.text(),
+            self.ui.priceSpinBox.value(),
+            self.ui.volumeSpinBox.value()
+        )
 
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        uic.loadUi("./main.ui", self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
-        self.con = sqlite3.connect("coffee.sqlite")
+        self.con = sqlite3.connect(DB_FILE)
 
+        self.ui.addButton.clicked.connect(self.add_coffee)
+        self.ui.editButton.clicked.connect(self.edit_coffee)
+
+        self.load_data()
+
+    def load_data(self):
         cursor = self.con.cursor()
-        data = cursor.execute("SELECT * FROM coffee").fetchall()
+
+        data = cursor.execute(
+            "SELECT * FROM coffee ORDER BY id"
+        ).fetchall()
 
         headers = [
             "ID",
@@ -34,13 +97,68 @@ class MyWidget(QMainWindow):
             items = [QStandardItem(str(value)) for value in row]
             model.appendRow(items)
 
-        self.tableView.setModel(model)
+        self.ui.tableView.setModel(model)
+        self.ui.tableView.resizeColumnsToContents()
 
-        self.tableView.resizeColumnsToContents()
+    def add_coffee(self):
+        dialog = AddEditCoffeeDialog(self)
+
+        if dialog.exec_() == QDialog.Accepted:
+            data = dialog.get_data()
+
+            cursor = self.con.cursor()
+
+            cursor.execute("""
+                INSERT INTO coffee
+                (name, roasting, type, taste, price, volume)
+                VALUES (?, ?, ?, ?, ?, ?)
+            """, data)
+
+            self.con.commit()
+            self.load_data()
+
+    def edit_coffee(self):
+        index = self.ui.tableView.currentIndex()
+
+        if not index.isValid():
+            return
+
+        row = index.row()
+
+        cursor = self.con.cursor()
+
+        coffee = cursor.execute(
+            "SELECT * FROM coffee ORDER BY id"
+        ).fetchall()[row]
+
+        dialog = AddEditCoffeeDialog(self, coffee)
+
+        if dialog.exec_() == QDialog.Accepted:
+            data = dialog.get_data()
+
+            cursor.execute("""
+                UPDATE coffee
+                SET name = ?,
+                    roasting = ?,
+                    type = ?,
+                    taste = ?,
+                    price = ?,
+                    volume = ?
+                WHERE id = ?
+            """, (*data, coffee[0]))
+
+            self.con.commit()
+            self.load_data()
+
+    def closeEvent(self, event):
+        self.con.close()
+        event.accept()
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+
     ex = MyWidget()
     ex.show()
-    sys.exit(app.exec())
+
+    sys.exit(app.exec_())
